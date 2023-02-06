6cb6ec0ec399a9ee3b827616e202c8af93575d8a
Vivie <xiaowei_1990617@126.com>
2023-02-06 11:49:52 +0800

    modified 04

--- 04_函数_通过用户选择实现代码.py
+++ 04_函数_通过用户选择实现代码.py
@@ -8,15 +8,21 @@ def user_info():
     print('用户信息')
 
 mapping = {
-    '1': registeer,
-    '2': login,
-    '3': user_info
+    '1': (registeer, '注册'),
+    '2': (login, '登录'),
+    '3': (user_info, '用户信息')
 }
 
-print('1.注册 2.登录 3.用户信息')
+line_list = []
+for k, v in mapping.items():
+    line = '{}.{}'.format(k, v[-1])
+    line_list.append(line)
+message = ' '.join(line_list)
+print(message)    
+
 choice = input('选择：')
 func = mapping.get(choice)
 if func:
-    func()
+    func[0]()
 else:
     print('选择错误')
